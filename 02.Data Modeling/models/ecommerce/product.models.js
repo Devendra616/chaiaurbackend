2b9329af0c02292f6ffb310358993580a8bf2cee
Devendra Singh <devendra.616@gmail.com>
2024-05-23 07:36:29 +0530

    data modeling practice

---
+++ 02.Data Modeling/models/ecommerce/product.models.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    productImages: {
+      type: String,
+    },
+    price: {
+      type: Number,
+      default: 0,
+    },
+    stock: {
+      type: Number,
+      default: 0,
+    },
+    category: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Category",
+      required: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  { timestamps }
+);
+
+export const Product = mongoose.model("Product", ProductSchema);
