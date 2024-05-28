180639a05544e977298f03a529432c5d68493c81
Devendra Singh <devendra.616@gmail.com>
2024-05-28 07:48:05 +0530

    create user and video models

---
+++ 03.video project/src/models/video.model.js
@@ -0,0 +1,41 @@
+import mongoose, { Schema } from "mongoose";
+
+const videoSchema = new mongoose.Schema(
+  {
+    videoFile: {
+      type: String,
+      required: true,
+    },
+    thumbnail: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number, //from cloudinary
+      required: true,
+    },
+    views: {
+      type: Number,
+      default: 0,
+    },
+    isPublished: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const video = mongoose.model("Video", videoSchema);
